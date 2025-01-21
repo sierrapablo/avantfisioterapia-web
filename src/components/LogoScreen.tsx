14363226d1f0410aa4ca42d03ffcfb182bcbc7d9
sierrapablo <pablo@metrio.es>
2025-01-21 15:39:54 +0100

    ✨ feature: Replace onClick by animation in main screen

--- src/components/LogoScreen.tsx
+++ src/components/LogoScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/LogoScreen.css'
 import avantLogo from '../assets/Avant_logo6Recurso-1_4x.png'
 
@@ -9,15 +9,23 @@ interface LogoScreenProps {
 const LogoScreen: React.FC<LogoScreenProps> = ({ onStart }) => {
   const [fadeOut, setFadeOut] = useState(false)
 
-  const handleClick = () => {
-    setFadeOut(true)
-    setTimeout(onStart, 1000)
-  }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true)
+      setTimeout(onStart, 1000)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [onStart])
 
   return (
     <div className={`logo-screen ${fadeOut ? 'fade-out' : ''}`}>
-      <div className='logo-container' onClick={handleClick}>
-        <img src={avantLogo} alt='Avant Fisioterapia Logo' className='logo' />
+      <div className='logo-container'>
+        <img
+          src={avantLogo}
+          alt='Avant Fisioterapia Logo'
+          className='logo pulse-animation'
+        />
       </div>
     </div>
   )
